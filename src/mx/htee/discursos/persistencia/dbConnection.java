570e58d17fdc813fc6a3695dd17557c25e1eab2a
Eduardo Enrique Hernández Tirado <lalonandez16@hotmail.com>
2018-04-02 12:31:48 -0700

    Update dbConnection.java

--- src/mx/htee/discursos/persistencia/dbConnection.java
+++ src/mx/htee/discursos/persistencia/dbConnection.java
@@ -18,8 +18,8 @@ import javax.swing.JOptionPane;
 public class dbConnection {
     
     private String db =   "beshopping_discurso"; 
-    private String user = "beshopping_lalo";
-    private String password = "Atalaya123";
+    private String user = "***";
+    private String password = "***";
     private String url = "jdbc:mysql://1a.ncomputers.org:3306/" + db;
     private Connection conn = null;
     
